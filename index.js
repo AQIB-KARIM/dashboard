16ac7e92aa2f22293e466b341cd4e27be93b5ab3
AQIB-KARIM <aqibnali@gmail.com>
2022-08-21 18:22:27 +0500

    all-tabs-working

---
+++ index.js
@@ -0,0 +1,72 @@
+// Dashboard Main Tabs
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    var dashboardNavText = document.getElementById('dashboardNavText');
+    var strategyNavText  = document.getElementById('strategyNavText');
+    var tdAmeritradeNavText = document.getElementById('tdAmeritradeNavText');
+
+    cityName === "Dashboard" ? dashboardNavText.style.display = 'block' : dashboardNavText.style.display = 'none';
+    cityName === "Strategy" ? strategyNavText.style.display = 'block' : strategyNavText.style.display = 'none';
+    cityName === "TD_Ameritrade" ? tdAmeritradeNavText.style.display = 'block' : tdAmeritradeNavText.style.display = 'none';
+
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+// Inner Dashboard Tabs
+
+function managedParameters(evt, params) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent1");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("paramlinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(params).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen1").click();
+
+
+
+
+
+
+
+
+
+// Console Closed
+document.onkeydown = function (e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
